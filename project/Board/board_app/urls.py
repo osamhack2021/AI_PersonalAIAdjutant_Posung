8a931ec6bbb10fd257be08c0363c00a5cfeb0460
chaeng123 <71381080+chaeng123@users.noreply.github.com>
2021-09-19 14:49:02 +0000

    날라간거 복구

---
+++ project/Board/board_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('post', views.post, name='post'),
+    path('post/<int:id>', views.detail, name='detail')
+]
---
+++ project/Board/board_app/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import *
+
+# Create your views here.
+def index(request):
+    boards = {'boards': Board.objects.all()}
+    return render(request, 'list.html', boards)
+
+def post(request):
+    if request.method == "POST":
+        author = request.POST['author']
+        title = request.POST['title']
+        content = request.POST['content']
+        board = Board(author=author, title=title, content=content)
+        board.save()
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'post.html')
+
+def detail(request, id):
+    try:
+        board = Board.objects.get(pk=id)
+    except Board.DoesNotExist:
+        raise Http404("Does not exist!")
+    return render(request, 'detail.html', {'board': board})
