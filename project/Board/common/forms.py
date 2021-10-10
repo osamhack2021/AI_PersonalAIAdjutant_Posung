7509e59b75c8e8bcccd96f7e9d539ee07ee07a69
chaeng123 <71381080+chaeng123@users.noreply.github.com>
2021-10-10 14:50:59 +0000

    .

--- project/Board/common/forms.py
+++ project/Board/common/forms.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+    rank = forms.CharField(label="계급")
 
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ("username", "password1", "password2", "email", "rank")
