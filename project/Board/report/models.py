a64e586d57e61ee1c08985d68104478aa289c7b1
chaeng123 <71381080+chaeng123@users.noreply.github.com>
2021-10-11 14:11:05 +0000

    update report

--- project/Board/report/models.py
+++ project/Board/report/models.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import User
 
 class Report(models.Model):
     author = models.CharField(max_length=10, null=False)
+    rank = models.CharField(max_length=10, null=False)
     title = models.CharField(max_length=100, null=False)
     content = models.TextField(null=False)
+    content2 = models.TextField(null=False)
+    content3 = models.TextField(null=False)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
--- project/Board/report/views.py
+++ project/Board/report/views.py
@@ -12,9 +12,12 @@ def report(request):
 def report_post(request):
     if request.method == "POST":
         author = request.POST['author']
+        rank = request.POST['rank']
         title = request.POST['title']
         content = request.POST['content']
-        report = Report(author=author, title=title, content=content)
+        content2 = request.POST['content2']
+        content3 = request.POST['content3']
+        report = Report(author=author, rank=rank title=title, content=content, content2=content2, content3=content3)
         report.save()
         return HttpResponseRedirect(reverse('report'))
     else:
